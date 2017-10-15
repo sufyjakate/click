2717428996c31c555ea4e33f2a7a59ddab7338ee
Unknown <abdulrehmanzafar90@gmail.com>
2017-10-15 19:41:58 +0200

    Update Rule Widget Structure

--- backend/api/models/rule/ruleWidgetModel.js
+++ backend/api/models/rule/ruleWidgetModel.js
@@ -3,24 +3,28 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var RuleWidgetSchema = new Schema({
-    name: {
-        type: String,
-        required: 'Kindly enter the name of the ruleWidget'
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now
-    },
-    status: {
-        type: [{
-            type: String,
-            enum: ['activated', 'deactivated', 'created', 'deleted', 'triggered' ]
-        }],
-        default: ['ON']
-    },
-    config: {
+    widgetType: {
         type: String
     },
+    widgetView: {
+        type: String
+    },
+    widgetTitle: {
+        type: String
+    },
+    x: {
+        type: String
+    },
+    y: {
+        type: String
+    },
+    width: {
+        type: String
+    },
+    height: {
+        type: String
+    },
+
 });
 
 module.exports = mongoose.model('RuleWidget', RuleWidgetSchema);
