0093f1b2744f419c40165a2de58bd6b0af0b4998
sufyjakate <sufyjakate@gmail.com>
2017-10-22 19:17:20 +0200

    changes

---
+++ UI/src/app/controllers/DeviceCtrl/AddDeviceController.js
@@ -0,0 +1,64 @@
+/**
+ * Created by sufyjakate on 21.10.17.
+ */
+
+angular
+    .module('app')
+    .config(function($mdThemingProvider) {
+
+        // Configure a dark theme with primary foreground yellow
+
+        $mdThemingProvider.theme('docs-dark', 'default')
+            .primaryPalette('blue')
+            .dark();
+
+    })
+    .controller('AddDeviceController', ['InterControllerCommunication', '$scope', '$mdDialog', '$http'
+    , AddDeviceController]);
+
+        function AddDeviceController(icc, $scope, $mdDialog, $http) {
+
+            $scope.states = ('ON OFF').split(' ').map(function(state) {
+                return {abbrev: state};
+            });
+
+            $scope.device = {
+                name: '',
+                DeviceID: '',
+                DeviceType: '',
+                DeviceStatus: ''
+            };
+
+
+
+            //console.log(state);
+            $scope.addDevice = function () {
+                console.log($scope.device);
+
+                //var newdevice = $scope.device;
+                //var myJSON = JSON.stringify($scope.device);
+
+                $http({
+                    url: 'http://localhost:3333/devices',
+                    method: 'POST',
+                    data: $scope.device,
+                    headers: {'Content-Type': 'application/json'}
+                }).then (function successcall(data, status, headers, config) {
+
+                    $scope.device = data;
+                    console.log('Getting the data');
+                    console.log($scope.device);
+
+                }, function errorcall(data, status, headers, config) {
+                    $scope.status = status;
+                    alert('Data Sending Failed');
+                });
+
+                $mdDialog.hide();
+            };
+
+            $scope.closeDevice = function () {
+              $mdDialog.hide();
+            }
+        }
+
