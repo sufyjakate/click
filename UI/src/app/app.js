b3f9972e1362f9ad80cccce888645dbe63de00c1
Abdul Rehman Zafar <abdulrehmanzafar90@gmail.com>
2017-10-30 11:31:48 +0100

    Fix bower Problem

--- UI/src/app/app.js
+++ UI/src/app/app.js
@@ -1,6 +1,8 @@
 (function(){
   'use strict';
 
-  angular.module('app', [ 'ngMaterial', 'interControllerCommunication', 'ngCookies', 'mdColorPicker']);
+  angular.module('app', [ 'ngMaterial', 'interControllerCommunication',
+      // 'ngCookies', 'mdColorPicker'
+  ]);
 
 })();
