d0fabc046090845230d30c8abe0d0c61c02e7d7d
Abdul Rehman Zafar <abdulrehmanzafar90@gmail.com>
2017-11-03 07:53:36 +0100

    1st level selected

--- UI/src/app/controllers/rule/widget/RuleCreationController.js
+++ UI/src/app/controllers/rule/widget/RuleCreationController.js
@@ -1,51 +1,341 @@
-(function(){
+(function () {
 
-  angular
-    .module('app')
-    .controller('RuleCreationController', [
-      '$timeout', '$q', 'countriesService',
-        RuleCreationController
-    ]);
+    angular
+        .module('app')
+        .controller('RuleCreationController', [
+            '$timeout', '$q', 'countriesService',
+            RuleCreationController
+        ]);
 
-  function RuleCreationController($timeout, $q, countriesService) {
-    var vm = this;
+    function RuleCreationController($timeout, $q, countriesService) {
+        var vm = this;
 
-    console.log("rule Creation controller");
+        console.log("rule Creation controller");
 
-      vm.options = {
-          content: 'Menu',
-          isOpen: true,
-          toggleOnClick: true,
-          items: [
-              {
-                  content: 'About',
-                  onclick: function () {console.log('About');}
-              }
-          ]
-      };
+        vm.options = {
+            content: 'Menu',
+            isOpen: true,
+            toggleOnClick: true,
+            items: [
+                {
+                    content: 'About',
+                    onclick: function () {
+                        console.log('About');
+                    }
+                }
+            ]
+        };
 
 
-      vm.items=['Temporal','Spatial','Situation','Communication','Service','Physical Entity','Operator'];
+        vm.ruleParameters = {
+            triggers: [
+                {
+                    id: 1,
+                    name: 'Temporal',
+                    icon: 'access_time',
+                    link: '',
+                    children: [
+                        {
+                            id: 1,
+                            name: 'Dates of Month',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Days of Week',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Times',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+                        ,
+                        {
+                            id: 1,
+                            name: 'Duration',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
 
-    vm.countries = countriesService.loadAll();
-    vm.selectedCountry = null;
-    vm.searchText = null;
-    vm.querySearch = querySearch;
-    vm.disableCaching = true;
+                    ]
+                },
+                {
+                    id: 1,
+                    name: 'Spatial',
+                    icon: 'location_on',
+                    link: '',
+                    children: [
+                        {
+                            id: 1,
+                            name: 'Dates of Month',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Days of Week',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Times',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+                        ,
+                        {
+                            id: 1,
+                            name: 'Duration',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
 
-    function querySearch (query) {
-      var results = query ? vm.countries.filter( createFilterFor(query) ) : [],
-        deferred;
-      deferred = $q.defer();
-      $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-      return deferred.promise;
+                    ]
+                },
+                {
+                    id: 1,
+                    name: 'Situation',
+                    icon: 'traffic',
+                    link: '',
+                    children: [
+                        {
+                            id: 1,
+                            name: 'Dates of Month',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Days of Week',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Times',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+                        ,
+                        {
+                            id: 1,
+                            name: 'Duration',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+
+                    ]
+                },
+                {
+                    id: 1,
+                    name: 'Communication',
+                    icon: 'chat',
+                    link: '',
+                    children: [
+                        {
+                            id: 1,
+                            name: 'Dates of Month',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Days of Week',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Times',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+                        ,
+                        {
+                            id: 1,
+                            name: 'Duration',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+
+                    ]
+                },
+                {
+                    id: 1,
+                    name: 'Service',
+                    icon: 'cloud',
+                    link: '',
+                    children: [
+                        {
+                            id: 1,
+                            name: 'Dates of Month',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Days of Week',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Times',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+                        ,
+                        {
+                            id: 1,
+                            name: 'Duration',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+
+                    ]
+                },
+                {
+                    id: 1,
+                    name: 'Physical Entity',
+                    icon: 'accessibility',
+                    link: '',
+                    children: [
+                        {
+                            id: 1,
+                            name: 'Dates of Month',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Days of Week',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Times',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+                        ,
+                        {
+                            id: 1,
+                            name: 'Duration',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+
+                    ]
+                },
+                {
+                    id: 1,
+                    name: 'Operator',
+                    icon: 'more',
+                    link: '',
+                    children: [
+                        {
+                            id: 1,
+                            name: 'Dates of Month',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Days of Week',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        },
+                        {
+                            id: 1,
+                            name: 'Times',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+                        ,
+                        {
+                            id: 1,
+                            name: 'Duration',
+                            icon: 'time',
+                            link: '',
+                            children: []
+                        }
+
+                    ]
+                },
+
+            ],
+            actions: []
+        }
+
+
+        vm.items = ['Temporal', 'Spatial', 'Situation', 'Communication', 'Service', 'Physical Entity', 'Operator'];
+
+
+        vm.leveloneItem = {};
+
+        vm.selectLevelOneItem = function (item) {
+            vm.leveloneItem = item;
+            console.log("1st level item : ");
+            console.log(item);
+
+        }
+
+
+
+        vm.countries = countriesService.loadAll();
+        vm.selectedCountry = null;
+        vm.searchText = null;
+        vm.querySearch = querySearch;
+        vm.disableCaching = true;
+
+        function querySearch(query) {
+            var results = query ? vm.countries.filter(createFilterFor(query)) : [],
+                deferred;
+            deferred = $q.defer();
+            $timeout(function () {
+                deferred.resolve(results);
+            }, Math.random() * 1000, false);
+            return deferred.promise;
+        }
+
+        function createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+            return function filterFn(state) {
+                return (state.value.indexOf(lowercaseQuery) === 0);
+            };
+        }
     }
-
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-      return function filterFn(state) {
-        return (state.value.indexOf(lowercaseQuery) === 0);
-      };
-    }
-  }
 })();
